ee0df4311f50a098832bf9b54476c0e938a53e7f
Capries <grg06ltd@gmail.com>
2024-02-04 00:33:25 +0300

    Add project files.

---
+++ Dictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Dictionary<Key, Value>
+    {
+        Key[] keys;
+        Value[] values;
+        public Dictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+
+        public void Add(Key id, Value name)
+        {
+            Key[] tempKey = keys;
+            Value[] tempValue = values;
+
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+            keys[keys.Length - 1] = id;
+            values[values.Length - 1] = name;
+        }
+
+        public int Count
+        {
+            get { return values.Length; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryÖdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> users = new Dictionary<int, string>();
+            users.Add(1, "Arif");
+            users.Add(2, "Tarık");
+            users.Add(3, "İsmail");
+            users.Add(3, "Engin");
+
+            foreach (Dictionary<int, string> User in users)
+            {
+                Console.WriteLine("ID:" + User.Keys + "  " + "Name:" + User.Values);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
